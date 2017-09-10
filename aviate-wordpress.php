821a168a8c8604b35747563ee21f72a4ae89fc48
Tim Neutkens <timneutkens@me.com>
2017-09-10 21:14:13 +0200

    Load javascript the correct way

--- aviate-wordpress.php
+++ aviate-wordpress.php
@@ -1,22 +1,31 @@
 <?php
+/*
+Plugin Name:  Aviate WordPress
+Description:  Aviate implementation for WordPress
+Version:      0.0.1
+Author:       Tim Neutkens
+Author URI:   https://github.com/timneutkens
+License:      MIT License
+*/
+
 namespace WeProvide\Aviate\WordPress;
-use WeProvide\Aviate\WordPresss\Bridge;
+use WeProvide\Aviate\WordPress\Bridge;
 
 function addFiles() {
     $aviate = new Bridge();
 
     $files = apply_filters('aviate_files', $aviate->getFiles(), $aviate);
 
-    foreach($files as $fileType) {
-        foreach($fileType as $key => $file) {
+    foreach($files as $fileType => $files) {
+        foreach($files as $key => $file) {
             if($fileType === 'js') {
-                wp_enqueue_script('aviate_'.$filetype.'_'.$key, $file);                
-                return;
+                wp_enqueue_script('aviate_'.$fileType.'_'.$key, $file, [], null);                
+                continue;
             }
 
             if($fileType === 'css') {
-                wp_enqueue_style('aviate_'.$filetype.'_'.$key, $file);
-                return;
+                wp_enqueue_style('aviate_'.$fileType.'_'.$key, $file, [], null);
+                continue;
             }
         }
     }
